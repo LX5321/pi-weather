04b3ad8ad91f5ee804c2bbfa4da13074ca2e26b0
Alexander Rodrigues <49933731+LX5321@users.noreply.github.com>
2019-08-23 18:50:55 +0530

    Update serversetup.sh

--- serversetup.sh
+++ serversetup.sh
@@ -1,2 +1,2 @@
-wget https://raw.githubusercontent.com/lx5321/pi-weather/php/master/index.php
-wget https://raw.githubusercontent.com/lx5321/pi-weather/php/master/conn.php
+wget https://raw.githubusercontent.com/lx5321/pi-weather/master/php/index.php
+wget https://raw.githubusercontent.com/lx5321/pi-weather/master/php/conn.php
