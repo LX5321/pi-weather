f2d00ae8db7dbec80a19117885f42c722cdf82a8
Alexander Rodrigues <49933731+LX5321@users.noreply.github.com>
2019-08-23 16:56:42 +0530

    Added DHT22 suppport.
    Added code for the DHT22 module to be connected at GPIO pin 4.

--- weather.py
+++ weather.py
@@ -1,2 +1,32 @@
-# Demo Commit
-import re
+# essential os operations
+import os
+# timing library
+import time
+# DHT22 Library
+import Adafruit_DHT
+
+# intitialise
+DHT_SENSOR = Adafruit_DHT.DHT22
+# connected DHT22 to pin 4 of raspberry pi
+DHT_PIN = 4
+
+try:
+#     open file in append mode
+    f = open('/home/pi/humidity.csv', 'a+')
+    if os.stat('/home/pi/humidity.csv').st_size == 0:
+            f.write('Date,Time,Temperature,Humidity\r\n')
+except:
+    pass
+
+while True:
+#   record humidity and temprature
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+#   data validation check
+    if humidity is not None and temperature is not None:
+        f.write('{0},{1},{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature, humidity))
+    else:
+#       error
+        print("Failed to retrieve data from humidity sensor")
+
+    time.sleep(30)       
